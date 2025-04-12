1cd07e53724b8c9b074949be984d8f2529284cc9
Shubham Bagwe <bagwe.sh@northeastern.edu>
2025-04-12 01:14:56 -0400

    zip code max 6 digits

--- src/com/staymaster/controllers/AddHotel.java
+++ src/com/staymaster/controllers/AddHotel.java
@@ -68,8 +68,8 @@ public class AddHotel {
             int availableRooms = Integer.parseInt(availableRoomsInput);
 
             // Validate numeric fields
-            if (zipcode <= 0 || zipCodeInput.length() != 6) {
-                showErrorAlert("Validation Error", "Zip Code must be a positive number and of 6 digits.");
+            if (zipcode <= 0 || zipCodeInput.length() <= 6) {
+                showErrorAlert("Validation Error", "Zip Code must be a positive number and of max 6 digits.");
                 return;
             }
             if (totalRooms <= 0) {
